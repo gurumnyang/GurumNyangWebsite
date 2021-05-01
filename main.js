319d214e12e055e21adfa7040f886b394067da21
구름냥 <62387796+gurumnyang@users.noreply.github.com>
2021-05-01 22:46:11 +0900

    .

--- main.js
+++ main.js
@@ -2,23 +2,28 @@
 const express = require('express'), http = require('http');
 const path = require('path');
 const static = require('serve-static');
-const consoleStamp = require('console-stamp')
+const consoleStamp = require('console-stamp');
 
 const app = express();
 
 app.set('port', 3000);
-app.set('view engine','ejs');
 app.use('/public',static(path.join(__dirname,'public')));
 
 //타임스탬프
 consoleStamp(console, ['yyyy/mm/dd HH:MM:ss.l']);
+
 
 http.createServer(app).listen(app.get('port'), function (){
     console.log(`Express 서버를 시작한거에요! `);
     console.log(`port:[${app.get('port')}]`);
 });
 app.get('/', function (req, res) {
-    res.redirect('/public/HTML/main.html');
+    res.redirect('public/HTML/main.html');
     console.log(`[/][${req.ip}][request]`);
 });
 
+app.get('/imfor', function (req,res) {
+   res.writeHead('200', {'Content-type':'text/html;charset=utf8'});
+   res.end(`<p>'ip': '${req.ip}'</p><p>'ips': '${req.ips}'</p><p>'user': '${req.user}'</p><p>'hostname': '${req.hostname}'</p><p>'url': '${req.url}'</p><p>'params': '${req.params}'</p>`);
+    console.log(`[/imfor][${req.ip}][request]`);
+});
