b9a9d3609cdeeb3486268a60cd5761c9e65a5669
구름냥 <62387796+gurumnyang@users.noreply.github.com>
2021-05-01 19:40:45 +0900

    .
    .

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
+//모듈 호출
 const express = require('express'), http = require('http');
 const path = require('path');
+const static = require('serve-static');
+const consoleStamp = require('console-stamp')
 
 const app = express();
 
 app.set('port', 3000);
 
+//타임스탬프
+consoleStamp(console, ['yyyy/mm/dd HH:MM:ss.l']);
+
 http.createServer(app).listen(app.get('port'), function (){
-    console.log('Express 서버를 시작한거에요! // port:'+app.get('port'))
+    console.log(`Express 서버를 시작한거에요! `);
+    console.log(`port:[${app.get('port')}]`);
 });
 
-app.use('/',)
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+    console.log(`[/][${req.ip}][request]`)
+});
+app.use('/public',static(path.join(__dirname,'public')));
