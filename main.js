19af048c7532e3b478cae54ea09a5e1a248a82c5
구름냥 <62387796+gurumnyang@users.noreply.github.com>
2021-05-02 03:56:30 +0900

    login.html 제작+수정

--- main.js
+++ main.js
@@ -4,9 +4,12 @@ const path = require('path');
 const static = require('serve-static');
 const consoleStamp = require('console-stamp');
 const app = express();
+const bodyParser = require('body-parser')
 
 app.set('port', 3000);
 app.use('/public',static(path.join(__dirname,'public')));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //타임스탬프
 consoleStamp(console, ['yyyy/mm/dd HH:MM:ss.l']);
@@ -25,4 +28,21 @@ app.get('/imfor', function (req,res) {
    res.writeHead('200', {'Content-type':'text/html;charset=utf8'});
    res.end(`<p>'ip': '${req.ip}'</p><p>'ips': '${req.ips}'</p><p>'user': '${req.user}'</p><p>'hostname': '${req.hostname}'</p><p>'url': '${req.url}'</p><p>'params': '${req.params}'</p>`);
     console.log(`[/imfor][${req.ip}][request]`);
+});
+
+app.get('/login', function (req, res) {
+    res.redirect('public/HTML/login.html');
+    console.log(`[/login][${req.ip}][request]`);
+});
+
+app.use((req, res, next) => {
+    console.log(`[/Login][${req.ip}][post]`);
+
+    let paramId = req.body.id || req.query.id;
+    let paramPassword = req.body.password || req.query.password;
+
+    res.writeHead('200', {'Content-type':'text/html;charset=utf8'});
+    res.write(`<p>당신의 아이디는 ${paramId}</p>`);
+    res.write(`<p>당신의 비밀번호는 ${paramPassword}</p>`);
+    res.end;
 });
