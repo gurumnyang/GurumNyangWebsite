4ce971a03dda93afbb3e4c1777534d7f6f4d6d3c
구름냥 <62387796+gurumnyang@users.noreply.github.com>
2021-05-07 12:13:04 +0900

    페이지 표시 방식 수정

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const expressSession = require('express-session');
-
+const rateLimit = require("express-rate-limit");
+const favicon = require('serve-favicon');
 
 const options = {
     key: fs.readFileSync(path.join(__dirname, 'private.key').toString()),
@@ -20,6 +21,8 @@ const options = {
 
 app.set('port', 443);
 app.use('/public',static(path.join(__dirname,'public')));
+app.use('/',static(path.join(__dirname,'robot.txt')));
+
 app.use('/', require('./router.js').router)
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -31,6 +34,16 @@ app.use(expressSession({
 
 }))
 
+//아이콘
+app.use(favicon(__dirname + '/public/image/favicon.ico'));
+
+// Apply to all requests
+app.use(rateLimit({
+        windowMs: 60*1000,
+        max: 30
+    })
+);
+
 
 //타임스탬프
 consoleStamp(console, ['yyyy/mm/dd HH:MM:ss.l']);
@@ -40,26 +53,29 @@ https.createServer(options, app).listen(process.env.SSL_PORT || 443, function ()
     console.log(`Express https 서버를 시작한거에요! `);
     console.log(`port:[${app.get('port')}]`);
 });
+
+//보조 http서버
 http.createServer(options, app).listen(process.env.SSL_PORT ||3000, function (){
     console.log(`Express http 보조 서버를 시작한거에요! `);
     console.log(`port:[${app.get('port')}]`);
 });
+//http 리다이렉트
 
-//보조 http서버
 
-app.use('/public/HTML/login.html',(req, res, next) => {
-    console.log(`[/Login][${req.ip}][post]`);
+
+app.post('/login-test',function(req, res, next) {
+
 
     let paramId = req.body.id || req.query.id;
     let paramPassword = req.body.password || req.query.password;
-
+    console.log(`[/Login][${req.ip}][POST][id:${paramId}][password:${paramPassword}]`);
     res.writeHead('200', {'Content-type':'text/html;charset=utf8'});
     res.write(`<p>당신의 아이디는 ${paramId}</p>`);
     res.write(`<p>당신의 비밀번호는 ${paramPassword}</p>`);
     res.end();
 });
-app.use('/public/HTML/main.html',(req, res,  next) => {
-    console.log(`[/main][${req.ip}][post]`);
+app.use('/main.html',function(req, res, next) {
+    console.log(`[/main][${req.ip}][POST]`);
     let paramContent = req.body.loginContent;
     if(paramContent.replace(/[^a-zA-Z ]/g, "") == ""){
         res.redirect('https://gurumnyang.kro.kr/imfor/error');
@@ -70,7 +86,4 @@ app.use('/public/HTML/main.html',(req, res,  next) => {
 
 
 
-app.get('/getCookie', function (req, res){
-    res.send(req.cookies);
-    console.log(req.cookies);
-});
+
--- router.js
+++ router.js
@@ -1,44 +1,83 @@
-const express = require('express'), http = require('https');
+const express = require('express'), http = require('http'), https = require('https');
 const path = require('path');
 const consoleStamp = require('console-stamp');
 const bodyParser = require('body-parser')
 const router = express.Router()
 const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+const fs = require('fs');
+router.use(cookieParser());
+router.use(bodyParser.urlencoded({extended: false}));
+router.use(bodyParser.json());
 
+const fileLoad = function(file){
+    return path.join(__dirname,`public/HTML/${file}`);
+}
+
+router.all('*',function (req,res, next){
+    let protocol = req.headers['x-forwarded-proto'] || req.protocol;
+    if (protocol == 'https'){
+        if(req.url != '/favicon.ico'){
+            console.log(`[${req.url}][${req.ip}][${req.method}]`)
+        }
+
+        next();
+    } else {
+        let from = `${protocol}://${req.hostname}${req.url}`;
+        let to = `https://${req.hostname}${req.url}`;
+        console.log(`[${req.url}][${req.ip}][${from} --> ${to}]`)
+        res.redirect(to);
+    }
+});
 
 router.get('/', function (req, res) {
-    res.redirect('public/HTML/main.html');
-    console.log(`[/][${req.ip}][request]`);
+    res.sendFile(fileLoad('main.html'));
 });
 
 router.get('/imfor', function (req,res) {
     res.writeHead('200', {'Content-type':'text/html;charset=utf8'});
-    res.end(`<p>'ip': '${req.ip}'</p><p>'ips': '${req.ips}'</p><p>'user': '${req.user}'</p><p>'hostname': '${req.hostname}'</p><p>'url': '${req.url}'</p><p>'params': '${req.params}'</p>`);
-    console.log(`[/imfor][${req.ip}][request]`);
+    res.end(`<p>'ip': '${req.ip}'</p><p>'ips': '${req.ips}'</p><p>'user': '${req.user}'</p><p>'hostname': '${req.hostname}'</p><p>'url': '${req.url}'</p><p>'params': '${req.params}'</p><p>'protocol': '${req.headers['x-forwarded-proto'] || req.protocol}'</p>`);
+
 });
 
 router.get('/login', function (req, res) {
-    res.redirect('public/HTML/login.html');
-    console.log(`[/login][${req.ip}][request]`);
+    res.sendFile(fileLoad('login.html'));
 });
+
+router.get('/cat', function(req, res){
+    res.sendFile(fileLoad('cat.html'));
+})
 
 router.get('/imfor/:name', function (req, res) {
     if(req.params.name == "error"){
-        res.send('<H1>이런! 특수문자만 적은 모양이군요!</H1>' +
+        res.send('<h1>이런! 특수문자만 적은 모양이군요!</h1>' +
             '<a href="https://gurumnyang.kro.kr/">돌아가기</a>');
-        res.end();
     }
-    res.send('<H1>' + req.params.name + ', Hello !</H1>' +
+    res.send('<h1>' + req.params.name + ', Hello !</h1>' +
         '<a href="https://gurumnyang.kro.kr/">돌아가기</a>');
-    res.end();
-})
+});
 
 router.get('/setCookie', function (req, res) {
-    res.cookie('json', {
+    res.cookie('setCookie-test', {
         id: 'gurumnyang',
         name: 'cookie'
     });
-    res.end();
-})
+    res.send('쿠키 설정이 완료되었습니다.')
+});
+router.get('/getCookie', function (req, res){
+    res.send(req.cookies);
+    console.log(req.cookies);
+});
+router.get('/search', function (req, res){
+    res.sendFile(fileLoad('search.html'));
+});
+router.get('/login-make', function(req, res){
+    res.cookie('login', {
+        id: 'user',
+        name: 'login'
+    });
+    console.log(`[${req.url}][${req.ip}][${req.method}][쿠키 지급됨]`);
+    res.send('<p>로그인 정보가 성공적으로 저장되었습니다! (아이디와 비번이 아니라 로그인 사실이 저장됩니다)</p><a href="https://gurumnyang.kro.kr/">돌아가기</a>');
+} );
 
 exports.router = router;
